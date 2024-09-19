d3eb83ac09667526bf54b5733795cacf88629472
Kyle Lamocha <20190017027@my.xu.edu.ph>
2024-09-19 15:09:30 +0800

    added new db

--- tracker_db (3).sql
+++ tracker_db (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2024 at 02:25 AM
+-- Generation Time: Sep 19, 2024 at 09:08 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -20,6 +20,72 @@ SET time_zone = "+00:00";
 --
 -- Database: `tracker_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_ID` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_ID`, `product_id`, `product_name`, `product_price`) VALUES
+(1, 2, 'Carbonara', 250);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest_tbl`
+--
+
+CREATE TABLE `guest_tbl` (
+  `g_id` int(11) NOT NULL,
+  `guest_name` varchar(50) NOT NULL,
+  `guest_timein` datetime NOT NULL,
+  `guest_status` enum('Regular','Student') NOT NULL,
+  `guest_rate` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guest_tbl`
+--
+
+INSERT INTO `guest_tbl` (`g_id`, `guest_name`, `guest_timein`, `guest_status`, `guest_rate`) VALUES
+(1, 'asd', '0000-00-00 00:00:00', 'Regular', 1),
+(2, 'ddd', '2024-09-04 13:38:00', 'Regular', 2),
+(3, 'f', '0000-00-00 00:00:00', 'Regular', 1),
+(4, 'r', '0000-00-00 00:00:00', 'Student', 33);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prod_ID` int(11) NOT NULL,
+  `prod_img` varchar(100) DEFAULT NULL,
+  `prod_name` varchar(255) DEFAULT NULL,
+  `prod_price` int(11) DEFAULT NULL,
+  `prod_stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`prod_ID`, `prod_img`, `prod_name`, `prod_price`, `prod_stock`) VALUES
+(2, 'diary-free-carbonara.jpg', 'Carbonara', 250, 0),
+(3, 'th.jpg', 'Milktea', 150, 4);
 
 -- --------------------------------------------------------
 
@@ -47,6 +113,24 @@ INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 --
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_ID`);
+
+--
+-- Indexes for table `guest_tbl`
+--
+ALTER TABLE `guest_tbl`
+  ADD PRIMARY KEY (`g_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`prod_ID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -56,6 +140,24 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cart_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `guest_tbl`
+--
+ALTER TABLE `guest_tbl`
+  MODIFY `g_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `prod_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
